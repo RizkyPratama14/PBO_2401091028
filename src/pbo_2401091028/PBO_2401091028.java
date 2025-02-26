32dac151311fee996a66e346b8793fbae1a3a474
LENOVO <LENOVO@DESKTOP-MOCC28M>
2025-02-26 11:54:36 +0700

    Update

---
+++ src/pbo_2401091028/PBO_2401091028.java
@@ -0,0 +1,18 @@
+
+package pbo_2401091028;
+
+/**
+ *
+ * @author LABP1KOMP
+ */
+public class PBO_2401091028 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Belajar Java");
+        System.out.println("Pemograman Berbasis Orientasi");
+    }
+    
+}
